1339867850dda9472747e8ef61b3cdc87f7c2605
pellekrogholt <pellekrogholt@gmail.com>
2014-03-29 19:11:54 +0100

    clean up

--- Applications/Android Application/20121004/src/itupku/genie/talkingbadge/SPPService.java
+++ Applications/Android Application/20121004/src/itupku/genie/talkingbadge/SPPService.java
@@ -6,9 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.UUID;
 
 import android.app.Notification;
